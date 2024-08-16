70e2b49081638f9cf3cf5f5439f85018eb462496
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-08-16 11:00:30 -0500

    Json server for audiophile ecomerce website app

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const server = jsonServer.create()
 
 const router = jsonServer.router('data.json')
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3001
 
 server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
+server.use(router)
+server.listen(port, () => {
+  console.log('JSON Server')
 })
